71359eb395b01f44d9ab6a9309abee50c2cf19aa
selmakarouri <169104862+selmakarouri@users.noreply.github.com>
2024-09-12 21:40:17 +0100

    Update tourusprt.js

--- tourusprt.js
+++ tourusprt.js
@@ -283,7 +283,7 @@ const RabatTPremiumButton = createButton("Rabat Tourism Premium", function() {
                 if (isVisibleSubVisa && isOkButtonClicked) {
                     console.log("VisaSubType " + i + " is visible");
                     var VisaSubType = $("#VisaSubType" + i).data("kendoDropDownList");
-                    VisaSubType.select(3); // 3 corresponds to 'Tourism'
+                    VisaSubType.select(18); // 18 corresponds to 'Tourism'
                     VisaSubType.trigger("change");
 
                     await new Promise(resolve => setTimeout(resolve, 100)); // Ajouter un délai de 100 ms
