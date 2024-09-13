f009634c9611a6f0efdc7784e55d5023b03b16f3
selmakarouri <169104862+selmakarouri@users.noreply.github.com>
2024-09-13 06:39:57 +0100

    Update tourusprt.js

--- tourusprt.js
+++ tourusprt.js
@@ -228,7 +228,7 @@ const RabatTPremiumButton = createButton("Rabat Tourism Premium", function() {
                 if (isVisibleLocation) {
                     console.log("Location " + i + " is visible");
                     var Location = $("#Location" + i).data("kendoDropDownList");
-                    Location.select(2); // 2 corresponds to 'Rabat'
+                    Location.select(1); // 1 corresponds to 'Rabat'
                     Location.trigger("change");
 
                     await new Promise(resolve => setTimeout(resolve, 100)); // Ajouter un délai de 100 ms
