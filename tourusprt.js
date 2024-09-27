152dc427c313b55ef5d3ee57fe54a2b0a405ba9a
selmakarouri <169104862+selmakarouri@users.noreply.github.com>
2024-09-27 17:10:55 +0100

    Update tourusprt.js

--- tourusprt.js
+++ tourusprt.js
@@ -79,7 +79,7 @@ if (window.location.href.startsWith(vtvUrlPrefix)) {
                 if (isVisibleVisa) {
                     console.log("VisaType " + i + " is visible");
                     var VisaType = $("#VisaType" + i).data("kendoDropDownList");
-                    VisaType.select(2); // 2 corresponds to 'Short Stay Visa'
+                    VisaType.select(1); // 1 corresponds to 'Short Stay Visa'
                     VisaType.trigger("change");
 
                     await new Promise(resolve => {
@@ -252,7 +252,7 @@ const RabatTPremiumButton = createButton("Rabat Tourism Premium", function() {
                 if (isVisibleVisa) {
                     console.log("VisaType " + i + " is visible");
                     var VisaType = $("#VisaType" + i).data("kendoDropDownList");
-                    VisaType.select(2); // 2 corresponds to 'Short Stay Visa'
+                    VisaType.select(1); // 1 corresponds to 'Short Stay Visa'
                     VisaType.trigger("change");
 
                     await new Promise(resolve => {
@@ -458,7 +458,7 @@ const RabatTPremiumButton = createButton("Rabat Tourism Premium", function() {
                 if (isVisibleSubVisa && isOkButtonClicked) {
                     console.log("VisaSubType " + i + " is visible");
                     var VisaSubType = $("#VisaSubType" + i).data("kendoDropDownList");
-                    VisaSubType.select(3); //3  corresponds to 'Tourism'
+                    VisaSubType.select(15); //15  corresponds to 'Tourism'
                     VisaSubType.trigger("change");
 
                     await new Promise(resolve => setTimeout(resolve, 100)); // Ajouter un délai de 100 ms
@@ -630,7 +630,7 @@ const RabatTPremiumButton = createButton("Rabat Tourism Premium", function() {
                 if (isVisibleSubVisa && isOkButtonClicked) {
                     console.log("VisaSubType " + i + " is visible");
                     var VisaSubType = $("#VisaSubType" + i).data("kendoDropDownList");
-                    VisaSubType.select(3); // 3 corresponds to 'Tourism'
+                    VisaSubType.select(15); // 15 corresponds to 'Tourism'
                     VisaSubType.trigger("change");
 
                     await new Promise(resolve => setTimeout(resolve, 100)); // Ajouter un délai de 100 ms
